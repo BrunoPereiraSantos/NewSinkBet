4ff0e23d67d0968258d5cc15d915824f91bb209c
Bruno Pereira <bruno.ps@live.com>
2014-02-22 21:01:22 -0300

    commit após formatação

---
+++ src/projects/BaseTest/nodes/messages/MBT.java
@@ -0,0 +1,14 @@
+package projects.BaseTest.nodes.messages;
+
+import sinalgo.nodes.messages.Message;
+
+public class MBT extends Message {
+
+	String a = "";
+	@Override
+	public Message clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
